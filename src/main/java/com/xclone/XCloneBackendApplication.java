ee73c2e66bd71c55a931490e822b8951ae7cf844
Kiff <kiffoh@gmail.com>
2026-01-22 20:30:34 +0000

    Initial Spring Boot setup

---
+++ src/main/java/com/xclone/XCloneBackendApplication.java
@@ -0,0 +1,13 @@
+package com.xclone;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class XCloneBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(XCloneBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/xclone/XCloneBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.xclone;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class XCloneBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
