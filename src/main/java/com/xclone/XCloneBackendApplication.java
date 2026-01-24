7f41842dc98c3ac45567884a10dc29a804a39035
kiff <kiffoh@gmail.com>
2026-01-24 12:05:59 +0000

    fix checkstyle

--- src/main/java/com/xclone/XCloneBackendApplication.java
+++ src/main/java/com/xclone/XCloneBackendApplication.java
@@ -3,11 +3,11 @@ package com.xclone;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+/** Main Spring Boot application for X-Clone backend. */
 @SpringBootApplication
 public class XCloneBackendApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(XCloneBackendApplication.class, args);
-	}
-
+  public static void main(String[] args) {
+    SpringApplication.run(XCloneBackendApplication.class, args);
+  }
 }
