1649917e5f1e41a0abf70390c19373f504a25db2
kiff <kiffoh@gmail.com>
2026-01-30 22:58:38 +0000

    add JpaAuditing

--- src/main/java/com/xclone/XCloneBackendApplication.java
+++ src/main/java/com/xclone/XCloneBackendApplication.java
@@ -2,9 +2,13 @@ package com.xclone;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 /** Main Spring Boot application for X-Clone backend. */
 @SpringBootApplication
+@EnableJpaAuditing
+@EnableJpaRepositories(basePackages = "com.xclone.repository")
 public class XCloneBackendApplication {
 
   public static void main(String[] args) {
