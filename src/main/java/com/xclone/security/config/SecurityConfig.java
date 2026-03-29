1607f04faa0a34b0dfbde7eab8e4cb63f9708723
Kiff <125292279+kiffoh@users.noreply.github.com>
2026-03-29 13:00:35 +0100

    remove graphql from permitted routes

--- src/main/java/com/xclone/security/config/SecurityConfig.java
+++ src/main/java/com/xclone/security/config/SecurityConfig.java
@@ -48,8 +48,7 @@ public class SecurityConfig {
                     "/v3/api-docs/**",
                     "/v3/api-docs.yaml",
                     "/swagger-ui/**",
-                    "/graphiql",
-                    "graphql")
+                    "/graphiql")
                 .permitAll()
                 .anyRequest()
                 .authenticated());
