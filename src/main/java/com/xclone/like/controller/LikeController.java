f61284593bbc3b8d9a8fcef1b81da8c000950a02
Kiff <125292279+kiffoh@users.noreply.github.com>
2026-04-27 10:19:45 +0100

    add like mutation operations

---
+++ src/main/java/com/xclone/like/controller/LikeController.java
@@ -0,0 +1,66 @@
+package com.xclone.like.controller;
+
+import com.xclone.exception.GraphQlErrorMapper;
+import com.xclone.exception.custom.PostNotFoundException;
+import com.xclone.like.service.LikeService;
+import com.xclone.post.dto.PostProfile;
+import com.xclone.post.dto.mutation.PostResponse;
+import com.xclone.post.service.PostService;
+import com.xclone.security.jwt.JwtAuthenticationFilter;
+import com.xclone.security.user.CustomUserDetails;
+import java.util.UUID;
+import org.springframework.graphql.data.method.annotation.Argument;
+import org.springframework.graphql.data.method.annotation.MutationMapping;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.stereotype.Controller;
+
+/** GraphQL controller for like entity operations. */
+@Controller
+public class LikeController {
+  LikeService likeService;
+  PostService postService;
+
+  public LikeController(LikeService likeService, PostService postService) {
+    this.likeService = likeService;
+    this.postService = postService;
+  }
+
+  /**
+   * Adds a like to a post on behalf of the authenticated user.
+   *
+   * @param userDetails authenticated user; populated as part of the security chain with {@link
+   *     JwtAuthenticationFilter}
+   * @param postId unique identifier of the post to add a like to
+   * @return updated post with the additional like
+   */
+  @MutationMapping
+  public PostResponse likePost(
+      @AuthenticationPrincipal CustomUserDetails userDetails, @Argument UUID postId) {
+    try {
+      likeService.createLike(postId, userDetails.getId());
+      PostProfile updatedPost = postService.getPost(postId);
+      return new PostResponse("201", true, updatedPost, null);
+    } catch (PostNotFoundException ex) {
+      return new PostResponse(
+          "400", false, null, GraphQlErrorMapper.fromPostNotFound("postId", ex));
+    }
+  }
+
+  /**
+   * Removes a like from a post on behalf of the authenticated user.
+   *
+   * <p>Unlike process fails silently if a post is not found.
+   *
+   * @param userDetails authenticated user; populated as part of the security chain with {@link
+   *     JwtAuthenticationFilter}
+   * @param postId unique identifier of the post to remove a like from
+   * @return updated post with the like removed
+   */
+  @MutationMapping
+  public PostResponse unlikePost(
+      @AuthenticationPrincipal CustomUserDetails userDetails, @Argument UUID postId) {
+    likeService.deleteLike(postId, userDetails.getId());
+    PostProfile updatedPost = postService.getPost(postId);
+    return new PostResponse("200", true, updatedPost, null);
+  }
+}
---
+++ src/main/java/com/xclone/like/model/LikeConstraintName.java
@@ -0,0 +1,7 @@
+package com.xclone.like.model;
+
+/** Constants representing business constraint names for likes. */
+public class LikeConstraintName {
+  public static final String LIKE_EXISTS = "like_exists";
+  public static final String LIKE_POST_FK = "like_post_fk";
+}
